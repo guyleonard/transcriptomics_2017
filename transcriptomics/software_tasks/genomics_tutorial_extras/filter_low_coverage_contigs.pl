debc52777771c57c479909108e4a63bb1ef81925
Guy Leonard <guy.leonard@gmail.com>
2017-08-29 15:22:10 +0100

    further removal on unneeded programs

--- transcriptomics/software_tasks/genomics_tutorial_extras/filter_low_coverage_contigs.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-while (<>) {
-  if (/^>/){ $out = $_ =~ m/length_([5-9]|\d{2})\d{2}.*_cov_\d\d+\./ } 
-  print if $out;
-}
-
--- transcriptomics/software_tasks/genomics_tutorial_extras/reduce_fasta_10x.pl
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/perl
-while (<>) {
-    $x++ if />/; 
-    print unless ($x - 7) % 10;
-}
